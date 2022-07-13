1419c9839a132aa6a0c0e2f3e18ee6fb8a4e1d98
Jds <deemsaer@gmail.com>
2022-07-13 17:57:45 +0300

    Base

---
+++ ChatClient/Program.cs
@@ -0,0 +1,253 @@
+﻿using System.Net.Http.Json;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+public static class Bootstrap
+{
+    static void Main(string[] args)
+    {
+        ChatConnection client = new ChatConnection();
+
+        client.OnMessageFromServer = messageObject =>
+        {
+            var dateTime = messageObject["utcTime"].GetValue<DateTime>();
+            var message = messageObject["message"].GetValue<string>();
+            var messageId = messageObject["messageId"].GetValue<int>();
+            Console.WriteLine($"{message} ({dateTime.ToLocalTime().ToShortTimeString()})");
+        };
+
+        client.SetMessageValidator(new LengthValidator(1, 128,
+            (message, maxLength) => 
+                Console.WriteLine($"Message {message} is too long. Max size is {maxLength} chars"),
+            (message, minLength) =>
+                Console.WriteLine($"Message {message} is too small. Min size is {minLength} chars")));
+        
+        client.SetNameValidator(new LengthValidator(4, 16,
+            (name, maxLength) => 
+                Console.WriteLine($"Name {name} is too long. Max size is {maxLength} chars"),
+            (name, minLength) =>
+                Console.WriteLine($"Name {name} is too small. Min size is {minLength} chars")));
+
+        while (!client.IsNameSet)
+        {
+            Console.WriteLine("Enter Name:");
+            string name = Console.ReadLine() ?? " ";
+            Ext.ConsoleLineBack();
+            client.SetName(name);
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Enter Message:");
+            string message = Console.ReadLine() ?? " ";
+            Ext.ConsoleLineBack(2);
+            client.Write(message);
+        }
+    }
+}
+
+public static class Ext
+{
+    public static void ConsoleLineBack(int linesCount = 1)
+    {
+        
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write(new string(' ', Console.BufferWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+    }
+}
+
+public static class ValidatorFactory
+{
+    public static IValidator GetDefaultNameValidator()
+    {
+       return new EmptyCharsValidator(new LengthValidator(4, 16,
+            (name, maxLength) =>
+                Console.WriteLine($"Name {name} is too long. Max size is {maxLength} chars"),
+            (name, minLength) =>
+                Console.WriteLine($"Name {name} is too small. Min size is {minLength} chars")));
+    }
+    
+    public static IValidator GetDefaultMessageValidator()
+    {
+        return new EmptyCharsValidator(new LengthValidator(1, 128,
+            (message, maxLength) => 
+                Console.WriteLine($"Message {message} is too long. Max size is {maxLength} chars"),
+            (message, minLength) =>
+                Console.WriteLine($"Message {message} is too small. Min size is {minLength} chars")));
+    }
+}
+
+public interface IValidator
+{
+    bool Validate(string value);
+}
+
+public class EmptyCharsValidator : IValidator
+{
+    private IValidator _validator;
+    public EmptyCharsValidator(IValidator validator)
+    {
+        _validator = validator;
+    }
+
+    public bool Validate(string value)
+    {
+        value = value.Trim();
+        return _validator.Validate(value);
+    }
+}
+
+public class NullValidator : IValidator
+{
+    public bool Validate(string value)
+    {
+        return true;
+    }
+}
+
+public class LengthValidator : IValidator
+{
+    private Action<string, int> _onToLongCallback;
+    private Action<string, int> _onToSmallCallback;
+    public LengthValidator(int minLength, int maxLength, 
+        Action<string, int> onToLongCallback, Action<string, int> onToSmallCallback)
+    {
+        _onToLongCallback = onToLongCallback;
+        _onToSmallCallback = onToSmallCallback;
+        _minLength = minLength;
+        _maxLength = maxLength;
+    }
+
+    private int _minLength;
+    private int _maxLength;
+    
+    public bool Validate(string value)
+    {
+        if (value.Length < _minLength)
+        {
+            _onToSmallCallback(value, _minLength);
+            return false;
+        }
+        if (value.Length > _maxLength)
+        {
+            _onToLongCallback(value, _maxLength);
+            return false;
+        }
+
+        return true;
+    }
+}
+
+public class ChatConnection
+{
+    private string _ip = "127.0.0.1";
+    private int _port = 26950;
+
+    private int _bufferSize = 256;
+    private int _maxMessageSize = 128;
+
+    private TcpClient _tcpClient;
+    private NetworkStream _networkStream;
+    private byte[] _receiveBuffer;
+
+    private bool _isNameSet;
+
+    public Action OnServerConnected;
+    public Action<JsonNode> OnMessageFromServer;
+    public Action OnServerDisconnected;
+    private int _maxNameLength = 12;
+    private int _maxMessageLength = 24;
+
+    private IValidator _nameValidator = new NullValidator();
+    public void SetNameValidator(IValidator validator) => _nameValidator = validator;
+    private IValidator _messageValidator = new NullValidator();
+    public void SetMessageValidator(IValidator validator) => _messageValidator = validator;
+
+    public ChatConnection()
+    {
+        _tcpClient = new TcpClient()
+        {
+            ReceiveBufferSize = _bufferSize,
+            SendBufferSize = _bufferSize
+        };
+        _receiveBuffer = new byte[_bufferSize];
+        _tcpClient.BeginConnect(_ip, _port, OnTcpConnect, null);
+    }
+
+    public bool IsNameSet => _isNameSet;
+
+    public void SetName(string name)
+    {
+        if (!_nameValidator.Validate(name))
+            return;
+
+        _isNameSet = true;
+        var messageObject = JsonSerializer.Serialize(new
+        {
+            setName = name
+        });
+        SendMessage(messageObject);
+    }
+    
+    public void Write(string message)
+    {
+        if (!_messageValidator.Validate(message))
+            return;
+        
+        var messageObject = JsonSerializer.Serialize(new
+        {
+            message = message
+        });
+        SendMessage(messageObject);
+    }
+
+    private void SendMessage(string message)
+    {
+        byte[] messageBytes = Encoding.Unicode.GetBytes(message);
+        _networkStream.Write(messageBytes);
+    }
+
+    private void OnTcpConnect(IAsyncResult ar)
+    {
+        _tcpClient.EndConnect(ar);
+        
+        if(!_tcpClient.Connected)
+            return;
+
+        _networkStream = _tcpClient.GetStream();
+        _networkStream.BeginRead(_receiveBuffer, 0, _bufferSize, OnNetworkStreamData, null);
+    }
+
+    private void OnNetworkStreamData(IAsyncResult ar)
+    {
+        try
+        {
+            int byteSize = _networkStream.EndRead(ar);
+            if (byteSize <= 0)
+            {
+                return;
+            }
+            _networkStream.BeginRead(_receiveBuffer, 0, _bufferSize, OnNetworkStreamData, null);
+
+            
+            byte[] _data = new byte[byteSize];
+            Array.Copy(_receiveBuffer, _data, byteSize);
+            string message = Encoding.Unicode.GetString(_data);
+            var jsonMessage = JsonObject.Parse(message);
+            RouteMessageFromServer(jsonMessage);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    private void RouteMessageFromServer(JsonNode message)
+    {
+        OnMessageFromServer(message);
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,207 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Http.Json;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace ChatServer;
+using ChatServer;
+
+public static class Bootstrap
+{
+    private static Server _server;
+    static void Main(string[] args)
+    {
+        Console.Title = "Chat Server";
+        _server = new Server(26950);
+        Console.ReadKey();
+    }
+}
+
+
+public class Server
+{
+    private int _port;
+    private TcpListener _tcpListener;
+
+    private List<Client> _clients = new();
+    private List<Message> _messages = new();
+
+    public Server(int port)
+    {
+        _port = port;
+
+        Console.WriteLine("Starting server...");
+
+        _tcpListener = new TcpListener(IPAddress.Any, _port);
+        _tcpListener.Start();
+        _tcpListener.BeginAcceptTcpClient(OnTcpConnection, null);
+        
+        Console.WriteLine($"Server started on ip {Dns.GetHostEntry(Dns.GetHostName()).AddressList[0].ToString()}, port {_port}");
+    }
+
+    private void OnTcpConnection(IAsyncResult result)
+    {
+        TcpClient _tcpClient = _tcpListener.EndAcceptTcpClient(result);
+        _tcpListener.BeginAcceptTcpClient(OnTcpConnection, null);
+
+        Client newClient = new Client(Guid.NewGuid(), this);
+        _clients.Add(newClient);
+
+        try
+        {
+            Thread clientThread = new Thread(() => newClient.Connect(_tcpClient));
+            clientThread.Start();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        
+        Console.WriteLine("Client connected: " + _tcpClient.Client);
+    }
+
+    public void Broadcast(Guid originClientId, string message)
+    {
+        var originClient = _clients.FirstOrDefault(client => client.Id == originClientId);
+        string username = originClient != null ? originClient.Username : "EmptyName"; 
+        var messageObject = new Message(originClientId, username, message);
+        _messages.Add(messageObject);
+        foreach (var client in _clients)
+        {
+            if(client.Id != originClientId)
+                client.SendMessage(messageObject);
+        }
+    }
+
+    public void OnDisconnected(Guid id)
+    {
+        _clients.RemoveAll(client => client.Id == id);
+        Console.WriteLine("User disconnected : " + id);
+    }
+}
+
+public class Client
+{
+    private Server _server;
+
+    private string? _name;
+    private Guid _id;
+    private TcpClient _tcpClient;
+    private NetworkStream _networkStream;
+    private byte[] _receiveBuffer;
+    
+    private int _bufferSize = 256;
+    private int _maxMessageSize = 128;
+
+    public bool IsConnected => _tcpClient != null && _tcpClient.Client != null;
+    public Guid Id => _id;
+    public string Username => _name;
+
+    public Client(Guid id, Server server)
+    {
+        _id = id;
+        _server = server;
+    }
+
+    public void Connect(TcpClient tcpClient)
+    {
+        _tcpClient = tcpClient;
+        _tcpClient.ReceiveBufferSize = _bufferSize;
+        _tcpClient.SendBufferSize = _bufferSize;
+        _networkStream = _tcpClient.GetStream();
+        _receiveBuffer = new byte[_bufferSize];
+        
+        _networkStream.BeginRead(_receiveBuffer, 0, _bufferSize, OnNetworkStreamData, null);
+    }
+
+    public async void SendMessage(Message message)
+    {
+        byte[] bytes = Encoding.Unicode.GetBytes(message.GetJson());
+        await _networkStream.WriteAsync(bytes);
+        Console.WriteLine($"Message sent from server to client id : {_id}, message : {message}");
+    }
+
+    private void OnNetworkStreamData(IAsyncResult receive)
+    {
+        try
+        {
+            int byteSize = _networkStream.EndRead(receive);
+            if (byteSize <= 0)
+            {
+                Disconnect();
+                return;
+            }
+            _networkStream.BeginRead(_receiveBuffer, 0, _bufferSize, OnNetworkStreamData, null);
+            
+            byte[] receivedBytes = new byte[_bufferSize];
+            Array.Copy(_receiveBuffer, receivedBytes, _bufferSize);
+
+            string message = Encoding.Unicode.GetString(receivedBytes, 0, byteSize);
+            JsonNode messageObject = JsonObject.Parse(message);
+            RouteMessageFromClient(messageObject);
+        }
+        catch (Exception e)
+        { 
+            Console.WriteLine("OnNetworkStreamData Exception: " + e);
+        }
+    }
+
+    private void RouteMessageFromClient(JsonNode message)
+    {
+        Console.WriteLine($"User {_id} : Server receive message: {message}");
+
+        var setName = message["setName"];
+        if (setName != null)
+        {
+            _name = setName.GetValue<string>();
+            return;
+        }
+
+        var messageBody = message["message"]?.GetValue<string>();
+        _server.Broadcast(_id, messageBody ?? "_empty");
+    }
+
+    private void Disconnect()
+    {
+        _networkStream.Close();
+        _tcpClient.Close();
+        _server.OnDisconnected(_id);
+    }
+}
+
+public class Message
+{
+    private static ulong _lastMessageId;
+    public static ulong NextMessageId => _lastMessageId++;
+
+    private Guid _originClientId;
+    private string _username;
+    private DateTime _utcTime;
+    private ulong _messageId;
+    private string _message;
+    
+    public Message(Guid originClientId, string username, string message)
+    {
+        _username = username;
+        _originClientId = originClientId;
+        _utcTime = DateTime.UtcNow;
+        _messageId = NextMessageId;
+        _message = message;
+    }
+
+    public string GetJson()
+    {
+        string json = JsonSerializer.Serialize(new
+        {
+            utcTime = _utcTime,
+            messageId = _messageId,
+            message = _message
+        });
+        return json;
+    }
+}
